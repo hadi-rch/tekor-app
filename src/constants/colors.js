e381906ae68767b886ac832c2c2bc7e3d9cb073a
Hadi <hadirich3@gmail.com>
2025-06-28 20:08:12 +0700

    feat: add COLORS constant for consistent color usage across the application

---
+++ src/constants/colors.js
@@ -0,0 +1,9 @@
+export const COLORS = {
+    primary: '#E85E5E',
+    white: '#FFFFFF',
+    black: '#000000',
+    gray: '#A0A0A0',
+    lightGray: '#F0F0F0',
+    text: '#333333',
+    borderColor: '#E0E0E0',
+};
