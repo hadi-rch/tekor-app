d14f2895242e93de32ba09d0f1cca663edcec07b
Hadi <hadirich3@gmail.com>
2025-06-29 23:36:45 +0700

    fix: remove SafeAreaProvider from App component for simplified structure

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
+    <>
       <AppNavigator />
-    </SafeAreaProvider>
+    </>
   );
 }
