f0468b41d5718c3d7810e9d7d15ca0882de3433d
Hadi <hadirich3@gmail.com>
2025-07-07 15:44:51 +0700

    feat: update API_URL to new ngrok endpoint for improved connectivity

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,4 @@
 // const API_URL = 'http://10.10.102.118:8082'; 
-const API_URL = 'https://0ab1-180-248-33-245.ngrok-free.app'; 
+const API_URL = 'https://b553-36-85-50-252.ngrok-free.app'; 
 
 export default API_URL;
